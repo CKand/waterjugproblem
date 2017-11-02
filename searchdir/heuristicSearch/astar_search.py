71afcd5523b726d297a243ef45204acc8f3f332d
Christine Kandalaft <christine.kandalaft@gmail.com>
2017-11-02 16:53:44 -0400

    Update astar_search.py

--- searchdir/heuristicSearch/astar_search.py
+++ searchdir/heuristicSearch/astar_search.py
@@ -1,3 +1,6 @@
+#import sys
+#sys.path.append('C:\\Users\\Christine\\Documents\\Year4\\CSI4106\\Bonus Assignment\\waterjugproblem-master\\waterjugproblem-master\\')
+
 from operator import attrgetter
 from searchdir.node import *
 from searchdir.util import PriorityQueue
@@ -5,48 +8,53 @@ from searchdir.util import PriorityQueue
 ## It must return the solution node and the number of visited nodes
 def astar_search(initialState):
     print('A* ------------------------------------')
-    #Initiate Priority Queue with A* function and push initial node
-    pqueue = PriorityQueue(fct="A*")
-    initNode = Node(initialState)
-    pqueue.enqueue(initNode)
-    #Initiate explored list
-    explored = []
-    # Initialize visited counter to count number of visited nodes
-    visited = 1
+    # create the inital queue/list to look through nodes
+    frontierPQ = PriorityQueue(fct="A*")
+    #add the inital state as a node to the priority queue
+    initialNode = Node(initialState)
+    frontierPQ.enqueue(initialNode)
+    #Initiate exploredNodes list
+    exploredNodes = []
 
-    while not pqueue.isEmpty():
-        #Pop the node from Queue with highest priority (smallest cost + heauristic property)
-        currentNode = pqueue.dequeue(None)
-        # While node dequeued explore node
+    # loop through nodes as long as long as queue is not empty
+    while not frontierPQ.isEmpty():
+        # we store current node after dequeuing from our priority queue, with the lowest priority
+        currentNode = frontierPQ.dequeue(None)
+        # explore the node to see if it is the goal
         if not currentNode.state.isGoal():
-            visited += 1
-            #If node is not Goal, add it to closed
-            explored.append(currentNode)
+            #add it to closed since it is not the goal
+            exploredNodes.append(currentNode)
 
-            #Generate successors from current node
-            for successorNodes in currentNode.expand():
+            #Create child nodes from current node
+            for childNodes in currentNode.expand():
                 #Remove a duplicate of successor node if it's in frontier with a higher cost
-                if pqueue.__contains__(successorNodes):
-                    nodeInOpen = pqueue.dequeue(successorNodes)
+                #################
+                #not sure what this does and not sure if we need to keep it for or case?? 
+                if frontierPQ.__contains__(childNodes):
+                    checkNodeOpen = frontierPQ.dequeue(childNodes)
                     #if new path is not less than old path, leave the frontier unchanged
-                    if successorNodes.f >= nodeInOpen.f:
-                        pqueue.enqueue(nodeInOpen)
+                    if childNodes.f >= checkNodeOpen.f:
+                        frontierPQ.enqueue(checkNodeOpen)
 
                 #If Successor Node in closed list and successorNode has lower f than node in closed, remove node from closed
-                inClosedNode = inExplored(successorNodes,explored)
-                if (inClosedNode):
-                    if (successorNodes.f < inClosedNode.f ):
-                        explored.remove(inClosedNode)
+                checkNodeClosed = inexploredNodes(childNodes,exploredNodes)
+                if (checkNodeClosed):
+                    if (childNodes.f < checkNodeClosed.f ):
+                        exploredNodes.remove(checkNodeClosed)
+                ################################
 
-                if not inExplored(successorNodes,explored) and not pqueue.__contains__(successorNodes):
-                    pqueue.enqueue(successorNodes)
+                #if not already in frontier or not alreadt in explored nodes, add to frontier 
+                if not inexploredNodes(childNodes,exploredNodes) and not frontierPQ.__contains__(childNodes):
+                    frontierPQ.enqueue(childNodes)
         else:
-            print("Nodes remaining in Frontier",pqueue.size())
-            return currentNode, visited
+            #### dontt hink we need this 
+            #print("Nodes remaining in Frontier",frontierPQ.size()) 
+            return currentNode, len(exploredNodes) # add one to length for the current node being checked
 
-def inExplored(nodeUnderStudy,explored):
-    for node in explored:
-        if nodeUnderStudy.state.equals(node.state):
+###################### this looks so obviously fishy but i dunno what to do to make it look unstoled
+def inexploredNodes(childNode,exploredNodes):
+    for node in exploredNodes:
+        if childNode.state.equals(node.state):
             return node
     return False
 
